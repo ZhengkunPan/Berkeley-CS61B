3b0b2b608524750a37077a8acbf05f26348a2b21
ZhengkunPan <zhkpan@g.ucla.edu>
2022-06-22 12:16:37 +0800

    finished Planet constructors

---
+++ proj0/Planet.java
@@ -0,0 +1,30 @@
+public class Planet {
+	public double xxPos;
+	public double yyPos;
+	public double xxVel;
+	public double yyVel;
+	public double mass;
+	public String imgFileName;
+
+	/** Constructor of Planet class*/
+	public Planet(double xP, double yP, double xV,
+		double yV, double m, String img) {
+		xxPos = xP;
+		yyPos = yP;
+		xxVel = xV;
+		yyVel = yV;
+		mass = m;
+		imgFileName = img;
+	}
+
+	/** Copy Planet class*/
+	public Planet(Planet p) {
+		xxPos = p.xxPos;
+		yyPos = p.yyPos;
+		xxVel = p.xxVel;
+		yyVel = p.yyVel;
+		mass = p.mass;
+		imgFileName = p.imgFileName;
+	}
+
+}
