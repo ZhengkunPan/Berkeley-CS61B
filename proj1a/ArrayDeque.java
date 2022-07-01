a0afb5cee775b95e9e365b172a8b08fae0704c1e
ZhengkunPan <zhkpan@g.ucla.edu>
2022-07-02 00:30:10 +0800

    debug LLD

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,43 +1,28 @@
 public class ArrayDeque<T> {
-
-    private class TNode {
-        public T item;
-        public T next;
-        public T previous;
-
-        public TNode() {
-            next = null;
-            previous = null;
-        }
-        public TNode(T i, T p, T n) {
-            item = i;
-            previous = p;
-            next = n;
-        }
-    }
     private int size;
     private T[] items;
     private int prevIndex;
     private int nextIndex;
-    private TNode sentinel;
 
     /** Creates an empty array deque.*/
     public ArrayDeque() {
         items = (T[]) new Object[8];
-        prevIndex =  items.length-1;
-        nextIndex = 0;
-        sentinel.previous = items[items.length-1];
-        sentinel.next = items[0];
+        prevIndex = 0;
+        nextIndex = 1;
         size = 0;
     }
 
     /** Adds an item of type T to the front of the deque.*/
     public void addFirst(T item) {
+        items[prevIndex] = item;
+        prevIndex -= 1;
         size += 1;
     }
 
     /** Adds an item of type T to the back of the deque.*/
     public void addLast(T item) {
+        items[nextIndex] = item;
+        nextIndex += 1;
         size += 1;
     }
 
@@ -64,16 +49,45 @@ public class ArrayDeque<T> {
 
     /** Removes and returns the item at the front of the deque. If no such item exists, returns null.*/
     public T removeFirst() {
+        items[prevIndex+1] = null;
+        prevIndex += 1;
         size -= 1;
     }
 
     /** Removes and returns the item at the back of the deque. If no such item exists, returns null.*/
     public T removeLast() {
+        items[nextIndex-1] = null;
+        nextIndex -= 1;
         size -= 1;
     }
 
-    /** Gets the item at the given index*/
+    /** Gets the item at the given index.*/
     public T get(int index) {
         return items[index];
     }
+
+    /** Calculates the index*/
+    public int calcIndex(int index) {
+        if (index < 0) {
+            return  items.length - 1;
+        }
+
+        if (index >= items.length) {
+            return 0;
+        }
+
+        return index;
+    }
+
+    /** Determines if array needs to be resized*/
+    public boolean deterResize() {
+        return (float) size / items.length >= 0.25;
+    }
+
+    /** Resizes the array.*/
+    public void resize(int capacity) {
+        T[] newArr = (T[]) new Object[size*4];
+
+    }
+
 }
